cabfc87d72eb7fd6eb682bb764ed841c022fd460
olgazinchenko <ozinchenko.pm@gmail.com>
2024-02-26 21:32:06 +0100

    Added a shadow to the buttons.

--- MathTrainer/TrainViewController.swift
+++ MathTrainer/TrainViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 final class TrainViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet var buttonsCollection: [UIButton]!
+    
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
             print(type)
+        }
+    }
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureButtons()
+    }
+    
+    // MARK: - Methods
+    private func configureButtons() {
+        // Add shadow
+        buttonsCollection.forEach { button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
         }
     }
 }
