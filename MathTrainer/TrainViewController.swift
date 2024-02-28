0e227b963f84c5fbcfe5096254a387837396098b
olgazinchenko <ozinchenko.pm@gmail.com>
2024-02-28 22:34:10 +0100

    Added question configure logic and added displaying correct and random answers on the left and right buttons.

--- MathTrainer/TrainViewController.swift
+++ MathTrainer/TrainViewController.swift
@@ -10,17 +10,47 @@ import UIKit
 final class TrainViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet var buttonsCollection: [UIButton]!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
     
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
-            print(type)
+            switch type {
+            case .add:
+                sign = "+"
+            case .subtract:
+                sign = "-"
+            case .multiply:
+                sign = "*"
+            case .divide:
+                sign = "/"
+            }
+        }
+    }
+    
+    private var firstNumber = 0
+    private var secondNumber = 0
+    private var sign: String = ""
+    
+    private var answer: Int {
+        switch type {
+        case .add:
+            return firstNumber + secondNumber
+        case .subtract:
+            return firstNumber - secondNumber
+        case .multiply:
+            return firstNumber * secondNumber
+        case .divide:
+            return firstNumber / secondNumber
         }
     }
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureQuestion()
         configureButtons()
     }
     
@@ -33,5 +63,23 @@ final class TrainViewController: UIViewController {
             button.layer.shadowOpacity = 0.4
             button.layer.shadowRadius = 3
         }
+        
+        let isRightButton = Bool.random()
+        let randomAnswer = Int.random(in: (answer - 10)...(answer + 10))
+        
+        rightButton.setTitle(isRightButton ? String(answer) : String(randomAnswer), for: .normal)
+        leftButton.setTitle(isRightButton ? String(randomAnswer) : String(answer), for: .normal)
+    }
+    
+    private func configureQuestion() {
+        firstNumber = Int.random(in: 1...99)
+        secondNumber = Int.random(in: 1...99)
+        let question: String = "\(firstNumber) \(sign) \(secondNumber) ="
+        
+        questionLabel.text = question
+    }
+    
+    private func getAnswer() -> Int {
+        return 0
     }
 }
