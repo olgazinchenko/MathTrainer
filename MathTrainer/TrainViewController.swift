66260fe83387f919d516356ae46232e94838620e
olgazinchenko <ozinchenko.pm@gmail.com>
2024-02-29 11:16:32 +0100

    Added highlighting of correct and incorrect answers on the buttons and added a count of attempts.

--- MathTrainer/TrainViewController.swift
+++ MathTrainer/TrainViewController.swift
@@ -33,6 +33,11 @@ final class TrainViewController: UIViewController {
     private var firstNumber = 0
     private var secondNumber = 0
     private var sign: String = ""
+    private var count: Int = 0 {
+        didSet {
+            print("Count: \(count)")
+        }
+    }
     
     private var answer: Int {
         switch type {
@@ -54,8 +59,22 @@ final class TrainViewController: UIViewController {
         configureButtons()
     }
     
+    // MARK: - IBActions
+    
+    @IBAction func leftAction(_ sender: UIButton) {
+        check(answer: sender.titleLabel?.text ?? "", for: sender)
+    }
+    
+    @IBAction func rightAction(_ sender: UIButton) {
+        check(answer: sender.titleLabel?.text ?? "", for: sender)
+    }
+    
     // MARK: - Methods
     private func configureButtons() {
+        let buttonsArray = [leftButton, rightButton]
+        buttonsArray.forEach { button in
+            button?.backgroundColor = .systemYellow
+        }
         // Add shadow
         buttonsCollection.forEach { button in
             button.layer.shadowColor = UIColor.darkGray.cgColor
@@ -65,7 +84,11 @@ final class TrainViewController: UIViewController {
         }
         
         let isRightButton = Bool.random()
-        let randomAnswer = Int.random(in: (answer - 10)...(answer + 10))
+        var randomAnswer: Int
+        repeat {
+            randomAnswer = Int.random(in: (answer - 10)...(answer + 10))
+        } while randomAnswer == answer
+
         
         rightButton.setTitle(isRightButton ? String(answer) : String(randomAnswer), for: .normal)
         leftButton.setTitle(isRightButton ? String(randomAnswer) : String(answer), for: .normal)
@@ -79,7 +102,19 @@ final class TrainViewController: UIViewController {
         questionLabel.text = question
     }
     
-    private func getAnswer() -> Int {
-        return 0
+    private func check(answer: String, for button: UIButton) {
+        let isRightAnswer = Int(answer) == self.answer
+        
+        button.backgroundColor = isRightAnswer ? .green : .red
+        
+        if isRightAnswer {
+            let isSecondAttempt: Bool = leftButton.backgroundColor == .red || rightButton.backgroundColor == .red
+                count += isSecondAttempt ? 0 : 1
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+                self?.configureQuestion()
+                self?.configureButtons()
+            }
+        }
     }
 }
